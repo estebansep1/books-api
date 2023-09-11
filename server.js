d01e32ccc1af6913b28ecd2bd1417b5d0f8b5c5e
Esteban Sepulveda <tebansd@icloud.com>
2023-09-10 20:22:06 -0700

    connected mongoDB

--- server.js
+++ server.js
@@ -7,17 +7,15 @@ const cors = require('cors')
 //CONFIGURATION
 require("dotenv").config();
 const PORT = process.env.PORT
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
 .then(() => {console.log('Connected to mongo: ', process.env.MONGO_URI) })
 
 //MIDDLEWEAR
 app.use(cors())
 app.use(express.json())
-
-
-
-
-
 
 
 app.get('/', (req, res)=> {
