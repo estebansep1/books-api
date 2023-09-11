e6c1b25be8307fc77a4f062ba953fa3701346987
Esteban Sepulveda <tebansd@icloud.com>
2023-09-10 20:13:54 -0700

    connected to mongoDB

--- server.js
+++ server.js
@@ -1,22 +1,28 @@
-require("dotenv").config();
+//DEPENDENCIES
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const cors = require('cors')
 
+//CONFIGURATION
+require("dotenv").config();
 const PORT = process.env.PORT
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {console.log('Connected to mongo: ', process.env.MONGO_URI) })
 
 //MIDDLEWEAR
+app.use(cors())
 app.use(express.json())
+
+
+
+
+
 
 
 app.get('/', (req, res)=> {
     res.send('Welcome to my booksAPI homework!')
 })
-
-
-
-
-
 
 // LISTEN FOR CONNECTION
 app.listen(process.env.PORT, () => {
